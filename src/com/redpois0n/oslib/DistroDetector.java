0805eda9867fcae14fb054e4a067e0742a5ed4d8
redpois0n <red@cmail.nu>
2015-03-11 13:10:21 +0100

    Begin work on distrodetector

---
+++ src/com/redpois0n/oslib/DistroDetector.java
@@ -0,0 +1,41 @@
+package com.redpois0n.oslib;
+
+import java.util.List;
+
+public class DistroDetector {
+	
+	public static Distro getDistro() {
+		Distro distro = Distro.UNKNOWN;
+		try {
+			String detect;
+			String release;
+			String codename;
+			
+			List<String> checkLSBrelease = Utils.readProcess(new String[] { "type", "-p", "lsb_release" });
+			
+			if (checkLSBrelease.size() > 0) {
+				List<String> lsbRelease = Utils.readProcess(new String[] { "lsb_release", "-irc" });
+				
+				for (String s : lsbRelease) {
+					String[] split = s.split(":");
+					String key = split[0].trim();
+					String value = split[1].trim();
+					
+					if (key.equals("Distributor ID")) {
+						detect = value;
+					} else if (key.equals("Release")) {
+						release = value;
+					} else if (key.equals("Codename")) {
+						codename= value;
+					}
+				}
+			}
+			
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		return distro;
+	}
+
+}

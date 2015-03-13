5619f7e2c24f7eb72fee2321a697dbaeade5a058
redpois0n <red@cmail.nu>
2015-03-13 08:05:37 +0100

    Checks os.name if can't read /var/run/dmesg.boot

--- src/com/redpois0n/oslib/bsd/FlavorDetector.java
+++ src/com/redpois0n/oslib/bsd/FlavorDetector.java
@@ -11,13 +11,30 @@ public class FlavorDetector {
 	public static Flavor detect() throws UnsupportedOperatingSystemException {
 		Flavor flavor = null;
 		
+		List<String> file = null;
+		
 		try {
-			List<String> file = Utils.readFile(new File("/var/run/dmesg.boot");
-			if (flavor != null) {
-				return flavor;
-			}
+			file = Utils.readFile(new File("/var/run/dmesg.boot"));				
 		} catch (Exception ex) {
 			ex.printStackTrace();
+		}
+		
+		if (file != null) {
+			for (String line : file) {
+				flavor = Flavor.getFlavorFromString(line);
+				
+				if (flavor != Flavor.UNKNOWN) {
+					return flavor;
+				}
+			}
+		} else {
+			String prop = System.getProperty("os.name");
+			
+			flavor = Flavor.getFlavorFromString(prop);
+			
+			if (flavor != Flavor.UNKNOWN) {
+				return flavor;
+			}
 		}
 		
 		return Flavor.UNKNOWN;

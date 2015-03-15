a9e98c73010fd5beade7708f93c85e0aeceaf802
redpois0n <red@cmail.nu>
2015-03-15 01:24:42 +0100

    OSX detection fixed

--- src/com/redpois0n/oslib/osx/OSX.java
+++ src/com/redpois0n/oslib/osx/OSX.java
@@ -8,10 +8,20 @@ import com.redpois0n.oslib.Utils;
 
 public class OSX {
 	
-	public static boolean detect(boolean fallback) {
-		File file = new File("/usr/bin/sw_vers");
-		
-		List<String> lines = Utils.readFile(file);
+	public static boolean detect(boolean fallback) {	
+		try {		
+			File file = new File("/usr/bin/sw_vers");
+			
+			List<String> lines = Utils.readFile(file);
+			
+			for (String line : lines) {
+				if (line.contains(OperatingSystem.OSX.getName())) {
+					return true;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		if (fallback) {
 			return System.getProperty("os.name").contains(OperatingSystem.OSX.getPrimarySearch());

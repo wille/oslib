3c54a38e5d9b41fe2cbfb839f65ccfb532578774
redpois0n <red@cmail.nu>
2015-07-16 11:56:11 +0200

    Comments

--- src/com/redpois0n/oslib/VersionCompare.java
+++ src/com/redpois0n/oslib/VersionCompare.java
@@ -2,7 +2,18 @@ package com.redpois0n.oslib;
 
 public abstract interface VersionCompare {
 	
+	/**
+	 * Checks if current version is newer than e
+	 * @param e
+	 * @return
+	 */
 	public abstract boolean isNewer(Enum<?> e);
+	
+	/**
+	 * Checks if current version is older than e
+	 * @param e
+	 * @return
+	 */
 	public abstract boolean isOlder(Enum<?> e);
 
 }
